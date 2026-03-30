f6f10f218d0da6d7120a17a866d3ef87ca52f22c
Ethan <mingam2007@gmail.com>
2026-03-30 09:27:05 +0200

    remove domain

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Documentations from "./Documentations.jsx"
 import Ecole from "./Ecole.jsx"
 import Entreprise from "./Entreprise.jsx"
 import Veilles from "./Veilles.jsx"
+import Apropos from "./Apropos.jsx"
+import img1 from "./assets/img1.webp"
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 
 function App() {
@@ -13,6 +15,7 @@ function App() {
                     <Link to="/">Acceuil</Link> |{" "}
                     <Link to="/documentations">Documentations</Link> |{" "}
                     <Link to="/veilles">Veilles</Link>
+                    <Link to="/apropos">Apropos</Link>
                 </nav>
 
                 <Routes>
@@ -21,8 +24,11 @@ function App() {
                     <Route path="/veilles" element={<Veilles />} />
                     <Route path="/ecole" element={<Ecole />} />
                     <Route path="/entreprise" element={<Entreprise />} />
+                    <Route path="/apropos" element={<Apropos />} />
                 </Routes>
             </BrowserRouter>
+
+            <img src="img1" alt="./assets/img1.webp" />
         </>
     )
 }
---
+++ src/Apropos.jsx
@@ -0,0 +1,29 @@
+function Apropos() {
+    return (
+        <>
+            <h3>Présentation du BTS SIO</h3>
+
+            <h4>BTS SIO (Services Informatiques aux Organisations)</h4>
+            <p>Le BTS SIO est un diplôme en informatique qui forme des techniciens capables de gérer, développer et sécuriser des systèmes informatiques en entreprise. La 
+formation se déroule généralement en deux ans après le bac. Elle propose deux spécialisations principales : SISR et SLAM. Les étudiants apprennent les bases des 
+réseaux, de la programmation, de la gestion de systèmes et de la cybersécurité.</p>
+
+            <h4>SISR (Solutions d’Infrastructure, Systèmes et Réseaux)</h4>
+            <p>L’option SISR est orientée vers l’administration des réseaux et des systèmes informatiques. Les étudiants apprennent à installer, configurer et maintenir des serveurs, 
+des réseaux et des équipements informatiques. Ils travaillent aussi sur la supervision des infrastructures et la gestion des incidents techniques. Cette spécialité prépare 
+à des métiers comme administrateur systèmes et réseaux ou technicien informatique.</p>
+
+            <h4>SLAM (Solutions Logicielles et Applications Métiers)</h4>
+            <p>L’option SLAM est centrée sur le développement d’applications et de logiciels. Les étudiants apprennent différents langages de programmation pour créer des 
+applications web, mobiles ou professionnelles. Ils travaillent également sur les bases de données, la conception de logiciels et la maintenance des applications. Cette 
+spécialité prépare à des métiers comme développeur web, programmeur ou concepteur d’applications.</p>
+
+            <h4>Cybersécurité</h4>
+            <p>La cybersécurité consiste à protéger les systèmes informatiques, les réseaux et les données contre les attaques et les intrusions. Dans le BTS SIO, elle est abordée 
+dans les deux spécialités afin de sensibiliser les étudiants aux risques informatiques. Les étudiants apprennent à sécuriser les réseaux, gérer les accès et détecter les 
+menaces. Cette compétence est de plus en plus demandée dans les entreprises face aux cyberattaques.</p>
+        </>
+    )
+}
+
+export default Apropos
